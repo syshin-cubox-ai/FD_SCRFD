e3d45df97fbe475f403a68a90a83cecf2bd4ac94
Seokyong Shin <syshin@cubox.ai>
2022-05-27 10:34:41 +0900

    tools: Fix onnxruntime providers issue

--- tools/scrfd.py
+++ tools/scrfd.py
@@ -82,7 +82,8 @@ class SCRFD:
         if self.session is None:
             assert self.model_file is not None
             assert osp.exists(self.model_file)
-            self.session = onnxruntime.InferenceSession(self.model_file, None)
+            self.session = onnxruntime.InferenceSession(self.model_file,
+                                                        providers=onnxruntime.get_available_providers())
         self.center_cache = {}
         self.nms_thresh = 0.4
         self._init_vars()
