8489ac3ec8c5066f3ce63e7049d19dcc391db85a
Seokyong Shin <syshin@cubox.ai>
2022-07-01 14:05:17 +0900

    scrfd2onnx: Refactor code

--- tools/scrfd2onnx.py
+++ tools/scrfd2onnx.py
@@ -87,57 +87,69 @@ def pytorch2onnx(
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert MMDetection models to ONNX')
-    parser.add_argument('config', help='test config file path')
-    parser.add_argument('checkpoint', help='checkpoint file')
-    parser.add_argument('input-img', type=str, default='../tests/data/t1.jpg', help='Images for input')
-    parser.add_argument('output-file', type=str, default='')
-    parser.add_argument('opset-version', type=int, default=11)
-    parser.add_argument('shape', type=int, default=[-1, -1], help='input image size')
-    parser.add_argument('mean', type=float, default=[127.5, 127.5, 127.5], help='used for preprocess input data')
-    parser.add_argument('std', type=float, default=[128.0, 128.0, 128.0], help='used for preprocess input data')
-    parser.add_argument('simplify', type=bool, default=True, help='use onnx-simplifier')
+    parser.add_argument('config', help='config file path')
+    parser.add_argument('checkpoint', help='checkpoint file path')
+    parser.add_argument('--dynamic', type=bool, default=True, help='use dynamic axes')
+    parser.add_argument('--simplify', type=bool, default=True, help='use onnx-simplifier')
     args = parser.parse_args()
+    print(args)
 
-    assert args.opset_version == 11, 'MMDet only support opset 11 now'
-    assert len(args.mean) == 3
-    assert len(args.std) == 3
+    # Create model and tensor data
+    input_config = {
+        'input_shape': (1, 3, 640, 640),
+        'input_path': 'tests/data/t1.jpg',
+        'normalize_cfg': {'mean': [127.5, 127.5, 127.5], 'std': [128.0, 128.0, 128.0]}
+    }
+    model, input_data = generate_inputs_and_wrap_model(args.config, args.checkpoint, input_config)
 
-    if len(args.shape) == 1:
-        input_shape = (1, 3, args.shape[0], args.shape[0])
-    elif len(args.shape) == 2:
-        input_shape = (1, 3) + tuple(args.shape)
+    # Define output file path
+    output_dir = 'onnx'
+    os.makedirs(output_dir, exist_ok=True)
+    cfg_name = os.path.splitext(os.path.basename(args.config))[0]
+    if args.dynamic:
+        output_file = os.path.join(output_dir, f'{cfg_name}.onnx')
     else:
-        raise ValueError('invalid input shape')
+        output_file = os.path.join(output_dir, f'{cfg_name}_static_axes.onnx')
 
-    if input_shape[2] <= 0 or input_shape[3] <= 0:
-        input_shape = (1, 3, 640, 640)
-        dynamic = True
-        print('set to dynamic input with dummy shape:', input_shape)
+    if args.simplify:
+        ori_output_file = output_file.replace('.onnx', '_ori.onnx')
     else:
-        dynamic = False
+        ori_output_file = output_file
 
-    normalize_cfg = {'mean': args.mean, 'std': args.std}
+    # Define input and output names
+    input_names = ['input.1']
+    output_names = ['score_8', 'score_16', 'score_32', 'bbox_8', 'bbox_16', 'bbox_32']
 
-    if len(args.output_file) == 0:
-        output_dir = '../onnx'
-        os.makedirs(output_dir, exist_ok=True)
-        cfg_name = os.path.basename(args.config)
-        pos = cfg_name.rfind('.')
-        cfg_name = cfg_name[:pos]
-        if dynamic:
-            args.output_file = os.path.join(output_dir, f'{cfg_name}.onnx')
-        else:
-            args.output_file = os.path.join(output_dir, f'{cfg_name}_shape{input_shape[2]}x{input_shape[3]}.onnx')
+    # If model graph contains keypoints strides add keypoints to outputs
+    if 'stride_kps' in str(model):
+        output_names += ['kps_8', 'kps_16', 'kps_32']
 
-    # convert model to onnx file
-    pytorch2onnx(
-        args.config,
-        args.checkpoint,
-        args.input_img,
-        input_shape,
-        args.opset_version,
-        args.output_file,
-        args.simplify,
-        dynamic,
-        normalize_cfg,
+    # Define dynamic axes for export
+    if args.dynamic:
+        dynamic_axes = {out: {0: 'N', 1: 'C'} for out in output_names}
+        dynamic_axes[input_names[0]] = {0: 'N', 2: 'H', 3: 'W'}
+    else:
+        dynamic_axes = None
+
+    torch.onnx.export(
+        model,
+        input_data,
+        ori_output_file,
+        input_names=input_names,
+        output_names=output_names,
+        opset_version=11,  # only work with version 11
+        dynamic_axes=dynamic_axes,
     )
+
+    if args.simplify:
+        from onnxsim import simplify
+        model = onnx.load(ori_output_file)
+        if args.dynamic:
+            input_shapes = {model.graph.input[0].name: list(input_config['input_shape'])}
+            model, check = simplify(model, input_shapes=input_shapes, dynamic_input_shape=True)
+        else:
+            model, check = simplify(model)
+        assert check, 'Simplified ONNX model could not be validated'
+        onnx.save(model, output_file)
+        os.remove(ori_output_file)
+    print(f'Successfully exported ONNX model: {output_file}')
