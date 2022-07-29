52f944e83467d841d33f6cbe080763adc92993b9
Seokyong Shin <syshin@cubox.ai>
2022-07-29 09:23:03 +0900

    Create convert_onnx_all.sh

---
+++ tools/convert_onnx_all.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+GPU=1
+GROUP=scrfd
+
+CUDA_VISIBLE_DEVICES="$GPU" python tools/scrfd2onnx.py ./configs/"$GROUP"/scrfd_1g.py ./work_dirs/scrfd_1g/model.pth
+CUDA_VISIBLE_DEVICES="$GPU" python tools/scrfd2onnx.py ./configs/"$GROUP"/scrfd_2.5g.py ./work_dirs/scrfd_2.5g/model.pth
+CUDA_VISIBLE_DEVICES="$GPU" python tools/scrfd2onnx.py ./configs/"$GROUP"/scrfd_2.5g_bnkps.py ./work_dirs/scrfd_2.5g_bnkps/model.pth
+CUDA_VISIBLE_DEVICES="$GPU" python tools/scrfd2onnx.py ./configs/"$GROUP"/scrfd_10g.py ./work_dirs/scrfd_10g/model.pth
+CUDA_VISIBLE_DEVICES="$GPU" python tools/scrfd2onnx.py ./configs/"$GROUP"/scrfd_10g_bnkps.py ./work_dirs/scrfd_10g_bnkps/model.pth
+CUDA_VISIBLE_DEVICES="$GPU" python tools/scrfd2onnx.py ./configs/"$GROUP"/scrfd_34g.py ./work_dirs/scrfd_34g/model.pth
+CUDA_VISIBLE_DEVICES="$GPU" python tools/scrfd2onnx.py ./configs/"$GROUP"/scrfd_500m.py ./work_dirs/scrfd_500m/model.pth
+CUDA_VISIBLE_DEVICES="$GPU" python tools/scrfd2onnx.py ./configs/"$GROUP"/scrfd_500m_bnkps.py ./work_dirs/scrfd_500m_bnkps/model.pth
